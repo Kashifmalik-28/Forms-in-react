e245d9aecbd1d19c12b148e0ddc9f9ec188c3914
Kashif Awan <mkashifawan92@gmail.com>
2025-11-24 22:49:49 +0500

    Video_No_33 Completed

---
+++ forms-in-react/src/expenseData.jsx
@@ -0,0 +1,16 @@
+export default [
+  { id: crypto.randomUUID(), title: 'Milk', category: 'Grocery', amount: 400 },
+  { id: crypto.randomUUID(), title: 'Shirt', category: 'Clothes', amount: 600 },
+  {
+    id: crypto.randomUUID(),
+    title: 'Vegetables',
+    category: 'Grocery',
+    amount: 900,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Electricity Bills',
+    category: 'Bills',
+    amount: 1200,
+  },
+]
